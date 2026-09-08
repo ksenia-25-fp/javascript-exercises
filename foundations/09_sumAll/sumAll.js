b6e182834b24ab4e61f30d7281359938c53d1f0f
ksenia-25-fp <226387090+ksenia-25-fp@users.noreply.github.com>
2026-09-08 13:00:13 +0200

    Add solution for 09-sumAll js exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let start = Math.min(min, max);
+    let end = Math.max(min, max);
+    let result = 0;
+    for (let i = start; i <= end; i++) {
+    result += i; 
+    }
+    return result;
 };
 
 // Do not edit below this line
